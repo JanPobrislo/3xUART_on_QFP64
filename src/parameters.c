49733d3132a6db6155e93b07c4991fcff69739b1
Jan Pobrislo <jap-git-hub@te2000.cz>
2026-03-24 17:27:38 +0100

    Pridany soubory parameter.c .h

---
+++ src/parameters.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+//#include <stdint.h>
+//#include <stdbool.h>
+#include "parameters.h"
+#include "uart1.h"
+
+tci_parameters param;
+
+void Parameters_Init(void) {
+	unsigned char n;
+
+	param.primary_net = 15;
+	param.next_time = 3;
+	param.next_rpt = 1;
+	param.error_rpt = 1;
+	param.pretime = 0;
+	param.deadtime = 0;
+	param.sys_tok = 0;
+
+	for (n=0; n<MAX_NETS; n++) {
+		param.netdau[n] = 0;
+	}
+
+	for (n=0; n<MAX_ROUTES; n++) {
+		param.route[n].net = 0;
+		param.route[n].path = 0;
+		param.route[n].dau = 0;
+		param.route[n].follow = 0;
+		param.route[n].error = 0;
+		param.route[n].revers = 0;
+	}
+
+	//---- Default pro ladeni
+	param.netdau[14] = 3;
+	param.route[0].net = 15;
+	param.route[0].path = 255;
+	param.route[0].dau = 255;
+	param.route[0].follow = 2;
+	param.route[0].error = 2;
+	param.route[0].revers = 2;
+}
+
+void Parameters_Show(void) {
+	unsigned char n;
+	char txt[250] = "";
+
+	sendStringUART1("\r\nParameters:\r\n");
+	sprintf(txt,"PRIMARY NET: %u\r\n",param.primary_net);
+	sendStringUART1(txt);
+	sprintf(txt,"NEXT TIME: %u\r\n",param.next_time);
+	sendStringUART1(txt);
+	sprintf(txt,"NEXT RPT : %u\r\n",param.next_rpt);
+	sendStringUART1(txt);
+	sprintf(txt,"ERROR RPT: %u\r\n",param.error_rpt);
+	sendStringUART1(txt);
+	sprintf(txt,"PRETIME  : %u\r\n",param.pretime);
+	sendStringUART1(txt);
+	sprintf(txt,"DEADTIME : %u\r\n",param.deadtime);
+	sendStringUART1(txt);
+	sprintf(txt,"SYS.TOK  : %u\r\n",param.sys_tok);
+	sendStringUART1(txt);
+
+	sendStringUART1("-------------------------------------------------\r\nNET:");
+	for (n=0; n<MAX_NETS; n++) {
+		sprintf(txt," %02u",n+1);
+		sendStringUART1(txt);
+	}
+	sendStringUART1("\r\nDAU:");
+	for (n=0; n<MAX_NETS; n++) {
+		if (param.netdau[n]!=0) {
+			sprintf(txt," %02u",param.netdau[n]);
+			sendStringUART1(txt);
+		}
+		else {
+			sendStringUART1(" --");
+		}
+	}
+	sendStringUART1("\r\n-------------------------------------------------");
+
+	sendStringUART1("\r\nROUTE: NET PTH DAU -> FLW ERR REV\r\n");
+//	for (n=0; n<MAX_ROUTES; n++) {
+	n=0;
+	while ((n<MAX_ROUTES)&&(param.route[n].path!=0)) {
+		sprintf(txt,"       %02u",param.route[n].net);
+		sendStringUART1(txt);
+		if (param.route[n].path==255) {
+			sendStringUART1("  * ");
+		}
+		else {
+			sprintf(txt,"  %02u",param.route[n].path);
+			sendStringUART1(txt);
+		}
+		if (param.route[n].dau==255) {
+			sendStringUART1("  * ");
+		}
+		else {
+			sprintf(txt,"  %02u",param.route[n].dau);
+			sendStringUART1(txt);
+		}
+		sprintf(txt,"  -> %02u",param.route[n].follow);
+		sendStringUART1(txt);
+		sprintf(txt,"  %02u",param.route[n].error);
+		sendStringUART1(txt);
+		sprintf(txt,"  %02u\r\n",param.route[n].revers);
+		sendStringUART1(txt);
+		n++;
+	}
+//	sendStringUART1("       -------------------------\r\n");
+	sendStringUART1("-------------------------------------------------\r\n");
+
+}
---
+++ src/parameters.h
@@ -0,0 +1,36 @@
+#ifndef PARAMETERS_H
+#define PARAMETERS_H
+
+#include <stdint.h>
+//#include <stdbool.h>
+
+#define MAX_NETS      15
+#define MAX_ROUTES    10
+
+typedef struct {
+	unsigned char net;
+	unsigned char path;
+	unsigned char dau;
+	unsigned char follow;
+	unsigned char error;
+	unsigned char revers;
+} tci_routes;
+
+typedef struct {
+	unsigned char primary_net;
+	unsigned char next_time;
+	unsigned char next_rpt;
+	unsigned char error_rpt;
+	unsigned char pretime;
+	unsigned char deadtime;
+	unsigned char sys_tok;
+	unsigned char netdau[MAX_NETS];
+	tci_routes    route[MAX_ROUTES];
+} tci_parameters;
+
+extern tci_parameters param;
+
+void Parameters_Init(void);
+void Parameters_Show(void);
+
+#endif
